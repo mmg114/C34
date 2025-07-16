aeedcaac7a954a8a7785e2e2496648a2c6162beb
Cristian Camilo <204278670+Camil2111@users.noreply.github.com>
2025-07-15 21:40:06 -0500

    Trabajo en clase - Cristian Ramírez - constructor full y clase Main

---
+++ CristianRamirez/Main.java
@@ -0,0 +1,13 @@
+
+
+public class Main {
+    public static void main(String[] args) {
+        Persona camilo = new Persona("Camilo", "Ramirez", 20, 1.75, 'M', 70);
+
+        camilo.caminar();
+        camilo.comer();
+        camilo.ejercitar();
+        camilo.hablar();
+        camilo.pensar();
+    }
+}
