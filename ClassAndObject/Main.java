0b2561916488abf3cb4f15d36dbbf16e02a61407
Mauricio-Munoz <mauricio.m@moabits.com>
2025-07-15 20:23:35 -0500

    trabajo en clase

--- ClassAndObject/Main.java
+++ ClassAndObject/Main.java
@@ -6,20 +6,15 @@ public class Main {
         Persona persona1 = new Persona("Cristian","Ramirez");
         Persona persona2 = new Persona("Edgar","Delgado");
         Persona persona3= new Persona("Esteban","Rojas",20);
-    
-
         System.out.println("Nombre: " + persona1.getNombre());
         System.out.println("Apellido: " + persona1.getApellido());
-
         persona1.setNombre("Juan");
-
         System.out.println("Nombre: " + persona1.getNombre());
-
         persona1.caminar();
         persona1.comer();
         persona1.ejercitar();
       
-    
+    //Llamar constructor con todos los atributos
         
    
    
